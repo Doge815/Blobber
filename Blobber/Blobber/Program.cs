8f184646e2e2d6313461b8fe6346a282a21a5e0f
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2020-05-10 21:24:06 +0200

    improved code

--- Blobber/Blobber/Program.cs
+++ Blobber/Blobber/Program.cs
@@ -6,37 +6,41 @@
     using System.IO;
     using System.Collections.Generic;
     using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+    using System.Runtime.CompilerServices;
+
     class Program
     {
         static void Main()
         {
             string path = "../../../";
-            var file = "ChickenTikkaMasala.jpg";
-            Console.WriteLine($"Loading {file}");
+            string file = "ChickenTikkaMasala.jpg";
+            DoStuff(path, file);
+        }
 
-            FileStream OriginalStream = new FileStream(path + file, FileMode.Open, FileAccess.Read);
-            Bitmap OriginalBitmap = new Bitmap(OriginalStream);
-
+        public static unsafe void DoStuff(string path, string file)
+        {
+#if false
+            Bitmap OriginalBitmap = new Bitmap(path + file);
             int Width = OriginalBitmap.Width;
             int Height = OriginalBitmap.Height;
-            
-            Bitmap BlobBitmap = new Bitmap(Width, Height);
-            Graphics BlobGraphics = Graphics.FromImage(BlobBitmap);
+
+            Bitmap BlobBitmap = new Bitmap(Width, Height, OriginalBitmap.PixelFormat);
 
             BitmapData OriginalData = OriginalBitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadOnly, OriginalBitmap.PixelFormat);
             BitmapData BlobData = BlobBitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, BlobBitmap.PixelFormat);
 
             int size = Math.Abs(OriginalData.Stride) * OriginalData.Height;
             byte[] Raw = new byte[size];
-            System.Runtime.InteropServices.Marshal.Copy(OriginalData.Scan0, Raw, 0, size);
+            Marshal.Copy(OriginalData.Scan0, Raw, 0, size);
 
             int ColorDepth = Raw.Length / (Width * Height * 3);
             if (ColorDepth != 1) throw new ArgumentException("");
 
             Color[,] Image = new Color[Width, Height];
-            for(int i = 0; i < Height; i++)
+            for (int i = 0; i < Height; i++)
             {
-                for(int u = 0; u < Width; u++)
+                for (int u = 0; u < Width; u++)
                 {
                     int position = 3 * u + 3 * i * Width;
                     Image[u, i] = Color.FromArgb(Raw[position], Raw[position + 1], Raw[position + 2]);
@@ -46,38 +50,78 @@
             ///
             /// insert manipulation here
             ///
+#endif
+            using var original = new EpicImage(new Bitmap(path + file));
+            using var target = new EpicImage(new Bitmap(original.Width, original.Height, original.PixelFormat));
 
-            byte[] BlobRaw = new byte[size];
-            GCHandle gch = GCHandle.Alloc(BlobRaw, GCHandleType.Pinned);
-            for(int i = 0; i < Height; i++)
+            for (int y = 0; y < original.Height; y++)
             {
-                for(int u = 0; u < Width; u++)
+                for (int x = 0; x < original.Width; x++)
                 {
-                    int position = 3 * u + 3 * i * Width;
-                    BlobRaw[position] = Image[u, i].R;
-                    BlobRaw[position + 1] = Image[u, i].G;
-                    BlobRaw[position + 2] = Image[u, i].B;
+                    var source = original[x, y];
+                    target[x, y] = source;
                 }
             }
 
-            unsafe
-            {
-                TypedReference tr = __makeref(BlobRaw[0]);
-                IntPtr ptr = **(IntPtr**)(&tr);
-                BlobData.Scan0 = ptr;
-            }
-
-            OriginalBitmap.UnlockBits(OriginalData);
-            BlobBitmap.UnlockBits(BlobData);
-
-            BlobBitmap.Save($"{path}blobber-{file}", ImageFormat.Jpeg);
-            gch.Free();
-            GC.Collect();
+            target.Bitmap.Save($"{path}blobber-{file}", ImageFormat.Jpeg);
         }
 
-        Color GetAverageColor(List<Color> colors)
+        Color GetAverageColor(ICollection<Color> colors)
         {
             return new Color();
+        }
+    }
+
+    public unsafe class EpicImage : IDisposable
+    {
+        public readonly Bitmap Bitmap;
+        public readonly BitmapData BitmapData;
+        public int Width => Bitmap.Width;
+        public int Height => Bitmap.Height;
+        public PixelFormat PixelFormat => Bitmap.PixelFormat;
+        private readonly int BytesPerPixel;
+        private readonly int HeightInPixels;
+        private readonly int WidthInBytes;
+        private readonly byte* Pixels;
+
+        public EpicImage(Bitmap bitmap)
+        {
+            Bitmap = bitmap;
+            BitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            BytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            HeightInPixels = BitmapData.Height;
+            WidthInBytes = BitmapData.Width * BytesPerPixel;
+            Pixels = (byte*)BitmapData.Scan0;
+        }
+
+        public byte this[int idx]
+        {
+            get => Pixels[idx];
+            set => Pixels[idx] = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private byte* IndexOfCoord(int x, int y) => Pixels + (y * BitmapData.Stride) + (x * BytesPerPixel);
+
+        public Color this[int x, int y]
+        {
+            get
+            {
+                var pixel = IndexOfCoord(x, y);
+                return Color.FromArgb(pixel[0], pixel[1], pixel[2]);
+            }
+            set
+            {
+                var pixel = IndexOfCoord(x, y);
+                pixel[0] = value.R;
+                pixel[1] = value.G;
+                pixel[2] = value.B;
+            }
+        }
+
+        public void Dispose()
+        {
+            Bitmap.UnlockBits(BitmapData);
         }
     }
 }
